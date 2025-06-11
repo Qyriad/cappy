daf0d2660118910e44de7b6f96a34b73fe38b929
Qyriad <qyriad@qyriad.me>
2025-06-11 13:08:17 +0200

    maint(nix): filter byPythonVersion to ones that eval

--- default.nix
+++ default.nix
@@ -5,32 +5,40 @@
 
 let
 	inherit (pkgs) lib;
-	base = python3Packages.callPackage ./package.nix { };
+	cappy = python3Packages.callPackage ./package.nix { };
 
 	foldToList = list: f: lib.foldl' f [ ] list;
 	pipeNullable = lib.foldl' (value: f: lib.mapNullable f value);
 	maybeGetAttrFrom = attrset: name: attrset."${name}" or null;
 	append = appendage: base: base + appendage;
 
-in base.overrideAttrs (prev: {
+	pyAttrOrNull = python: let
+		res = builtins.tryEval (python.pythonAttr or null);
+	in if res.success then res.value else null;
+
+	pyInterpreters = lib.attrValues pkgs.pythonInterpreters;
+
+	optionalValidPython = python: let
+		pyAttr = pyAttrOrNull python;
+		isPy3 = python.isPy3 or false;
+		scope = pipeNullable pyAttr [
+			(append "Packages") # python312 -> python312Packages
+			(maybeGetAttrFrom pkgs) # python312Packages -> pkgs.python312Packages
+		];
+		isValid = scope != null && isPy3;
+	in lib.optional isValid {
+		name = scope.python.pythonAttr;
+		value = scope.callPackage ./package.nix { };
+	};
+
+	byPythonVersion = let
+		listOfCappyPairs = foldToList pyInterpreters (acc: python: acc ++ optionalValidPython python);
+		nonDisabledCappyPairs = lib.filter ({ name, value }: !value.meta.disabled) listOfCappyPairs;
+	in lib.listToAttrs nonDisabledCappyPairs;
+
+in cappy.overrideAttrs (prev: {
 	passthru = lib.recursiveUpdate (prev.passthru or { }) {
-		byPythonVersion = let
-			pyInterpreters = lib.attrValues pkgs.pythonInterpreters;
-			listOfAttrs = foldToList pyInterpreters (acc: python: let
-				attr = let
-					res = builtins.tryEval (python.pythonAttr or null);
-				in if res.success then res.value else null;
-				isPy3 = python.isPy3 or false;
-				scope = pipeNullable attr [
-					(append "Packages")
-					(maybeGetAttrFrom pkgs)
-				];
-				isValid = scope != null && isPy3;
-			in acc ++ lib.optional isValid {
-				name = scope.python.pythonAttr;
-				value = scope.callPackage ./package.nix { };
-			});
-		in lib.listToAttrs listOfAttrs;
+		inherit byPythonVersion;
 	};
 })
 
